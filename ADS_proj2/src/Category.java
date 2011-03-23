6e172f2282e5d44c8f99935338dce2f7e790ed14
tplee923@gmail.com <tplee923@gmail.com@9c05c0d7-0d30-947c-f193-bd0fc0f35499>
2011-03-23 19:47:43 +0000

    init version

---
+++ ADS_proj2/src/Category.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+
+public class Category {
+	private boolean isLeaf;
+	private String name;	//	store category's name
+	private ArrayList<Category> subcat;	//	the subcategories of this category
+	private String [] queries;
+	private Category parent;
+	
+	public Category(String name){
+		this.name = name;
+		//System.arraycopy(queries, 0, this.queries, 0, queries.length);
+	}
+	
+	public boolean getisLeaf(){
+		return isLeaf;
+	}
+	public void setisLeaf(Boolean b){
+		this.isLeaf = b;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public ArrayList<Category> getSubcat(){
+		return subcat;
+	}
+	public void setSubcat(ArrayList<Category> subcat){
+		this.subcat = subcat;
+	}
+	
+	public String [] getQueries(){
+		return queries;
+	}
+	
+	public void setQueries(String [] queries){
+		if (queries != null) {
+			int i = queries.length;
+			//System.out.println(this.name);
+			//System.out.println(i);
+			this.queries = new String[i];
+			System.arraycopy(queries, 0, this.queries, 0, queries.length);
+		}
+	}
+	
+	public Category getParent(){
+		return parent;
+	}
+	
+	public void setParent(Category parent) {
+		this.parent = parent;
+	}
+	
+}
+
